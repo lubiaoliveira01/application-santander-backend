fcac52dff89ff78901404ee0f96e73057c14b727
Lúbia Oliveira <lubia.oliveira01@gmail.com>
2021-06-13 20:14:06 -0300

    Adicionando backend

---
+++ src/test/java/com/project/backendsantander/BackendSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.backendsantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
